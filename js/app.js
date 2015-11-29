708ac90e8cc0a1e32a2738d0aa8e6871b71b59b4
VY <khi.chan.yang@gmail.com>
2015-11-30 00:00:19 +0300

    Polished some syntax

--- js/app.js
+++ js/app.js
@@ -2,23 +2,38 @@ var enemyNumberTotal = 3;
 var allEnemies = [];
 var allFriends = [];
 var allGems = [];
+
 var rawLocations = [-25, 55, 135, 215, 295, 375];
 var columnLocations = [0, 100, 200, 300, 400];
+
 var bugCount = 0;
 var timeForFriend;
 var score = 0, scoreRecord = 0;
 
-var gemSprites = ['images/Gem Blue.png', 'images/Gem Green.png', 'images/Gem Orange.png'];
+var playerSprites = [
+    'images/char-boy.png',
+    'images/char-cat-girl.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png'
+    ];
+
+var gemSprites = [
+    'images/Gem Blue.png',
+    'images/Gem Green.png',
+    'images/Gem Orange.png'
+    ];
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function() {
     this.x = 0;
     this.y = rawLocations[randomInteger(1,3)];
     this.speed = randomInteger(1,4);
     this.sprite = 'images/enemy-bug.png';
 };
 
-Enemy.prototype.update = function(dt, speed) {
+
+//When enemy reaches raw end, it reappears on another randomized line.
+Enemy.prototype.update = function(dt) {
     if (this.x > 500) {
         if (allEnemies.length <= enemyNumberTotal) {//Respawn bug code
             this.x = columnLocations[0];
@@ -28,11 +43,13 @@ Enemy.prototype.update = function(dt, speed) {
         else {
             allEnemies.splice(find(allEnemies, this), 1);
         };
+
         bugCount++;
         if (bugCount == timeForFriend) {
             createFriend();
         };
     };
+
     this.x = this.x + this.speed * dt * 100;
 };
 
@@ -40,58 +57,53 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var createEnemy = function (enemyNumber) {
-    for (enemyNumber = 0; enemyNumber < enemyNumberTotal ; enemyNumber++) {
-        allEnemies[enemyNumber] = new Enemy ();
+var createEnemy = function(num) {
+    for (i = enemyNumberTotal - num; i < enemyNumberTotal ; i++) {
+        allEnemies.push(new Enemy());
     }
 };
 
-
-
-
 //Friends code goes here:
-var Friend = function (x, y, speed) {
-    Enemy.call(this, x, y, speed);
+var Friend = function() {
+    Enemy.call(this);
     this.sprite = 'images/char-princess-girl.png';
 };
+
 Friend.prototype = Object.create(Enemy.prototype);
 Friend.prototype.constructor = Enemy;
 
-Friend.prototype.update = function () {
+Friend.prototype.update = function(dt) {
     if (this.x > 500) {
       friendSetTimer();
       allFriends=[];
     };
-    this.x = this.x + this.speed ; // NOTE: Can't use dt here ?! It's not defined!
+
+    this.x = this.x + this.speed * dt *100;
 };
 
-var createFriend = function () {
+var createFriend = function() {
     var friend = new Friend;
     allFriends.push(friend);
 };
 
-var friendSetTimer = function () {
-    timeForFriend = bugCount + randomInteger(3,15);
+var friendSetTimer = function() {
+    timeForFriend = bugCount + randomInteger(3,12);
 };
 
 //Player related code
-var player = function (x, y, playerType) {
-    this.x = x;
-    this.y = y;
-    this.sprite = playerSprites[playerType];
+var player = function() {
+    this.x = 200;
+    this.y = 375;
+    this.sprite = playerSprites[randomInteger(0,3)];
 };
-
-var playerSprites = ['images/char-boy.png', 'images/char-cat-girl.png',
-                 'images/char-horn-girl.png','images/char-pink-girl.png'];
 
 player.prototype.update = function() {
     //When player reaches the water, game restarts with 1 more enemy
     if (this.y == rawLocations[0]) {
-        allEnemies = [];
         this.y = 375;
         enemyNumberTotal++;
-        createEnemy(enemyNumberTotal);
-        score += 5;
+        createEnemy(1);
+        score += 50;
     };
 
     //When bug hits player, player goes back to start position
@@ -102,9 +114,9 @@ player.prototype.update = function() {
             if (score > 0) {
                 score -= 5;
             };
-            if (enemyNumberTotal > 3) {//TODO: Replace 3 with some variable
-                enemyNumberTotal -= 1;
-            }
+            // if (enemyNumberTotal > 3) {
+            //     enemyNumberTotal -= 1;
+            // }
         }
     };
 
@@ -123,6 +135,9 @@ player.prototype.update = function() {
             }
             allGems.splice(i, 1);
             friendSetTimer();
+            if (enemyNumberTotal > 3) {//TODO: Replace 3 with some variable
+                enemyNumberTotal -= 1;
+            }
         }
     };
 
@@ -131,9 +146,6 @@ player.prototype.update = function() {
         if (collision(this.x, this.y, allFriends[i].x, allFriends[i].y)) {
           allFriends=[];
           new Gem();
-            if (enemyNumberTotal > 3) {//TODO: Replace 3 with some variable
-                enemyNumberTotal -= 1;
-            }
         }
     }
 };
@@ -169,9 +181,8 @@ player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
 //Gem code goes here:
-var Gem = function () {
+var Gem = function() {
   this.x = columnLocations[randomInteger(0,4)];
   this.y = rawLocations[randomInteger(1,5)];
   this.sprite = gemSprites[randomInteger(0,2)];
@@ -182,10 +193,8 @@ Gem.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-
 //Actual creation of player, enemies, friends
-var player = new player(200, 375, randomInteger(0,3));
+var player = new player();
 createEnemy(enemyNumberTotal);
 createFriend();
 
@@ -197,21 +206,21 @@ function randomInteger(min, max) {
 };
 
 //Collision check
-var collision = function (playerX, playerY, x2, y2) {
+var collision = function(playerX, playerY, x2, y2) {
   var circle1 = {radius: 20, x: playerX, y: playerY};
   var circle2 = {radius: 20, x: x2, y: y2};
-
   var dx = circle1.x - circle2.x;
   var dy = circle1.y - circle2.y;
   var distance = Math.sqrt(dx * dx + dy * dy);
-
   if (distance < circle1.radius + circle2.radius) {
       return true;
   }
   return false;
 };
 
-var scoreUpdate = function () {
+
+// Code below is to keep track of score and to display it
+var scoreUpdate = function() {
     if (score > scoreRecord) {
         scoreRecord = score;
     }
@@ -221,10 +230,8 @@ var renderScore = function() {
     ctx.fillStyle = "#FFFFFF";
     ctx.font = "16pt Arial";
     ctx.textAlign = "end";
-    ctx.fillText("Player: " + score, 480, 80);
-    ctx.fillText("Record: " + scoreRecord, 480, 100);
-    // ctx.strokeStyle = "black"
-    // ctx.strokeText("Record: " + scoreRecord, 400, 100);
+    ctx.fillText("Player: " + score, 450, 80);
+    ctx.fillText("Record: " + scoreRecord, 450, 105);
 };
 
 // This listens for key presses and sends the keys to your
