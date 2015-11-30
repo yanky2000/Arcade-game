c9eb28ad7c22f7366008087548ea878a5f2538e8
VY <khi.chan.yang@gmail.com>
2015-11-30 03:08:57 +0300

    With README and License. Add self-explaining variables

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-frontend-nanodegree-arcade-game
+# Frogger Game clone
 ===============================
+In this game you have a Player and Enemies (Bugs).
+The goal of the player is to score as much points as possible.
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
-for self-checking their submission.
+## Game launch
+
+To run the application just open _index.html_ file in your browser (Google Chrome preferebly)
+
+## How to play
+
+Use arrow keys to move your player (up, down, left, right).
+
+## Rules
+
+Your goal is to reach the water. When you do, you receive 50 points. After that, player returns to the start square for another, a slightly more difficult round.
+
+If a BUG hits you on the road, you loose 5points and return to the start square.
+
+From time to time you will see a Princess passing by, she's your **Friend**. Try to catch up with her, and you'll get a GEM appeared on the screen for you as a reward. Only Princess makes gems.
+
+Collecting GEMS is awesome! Not only it gives you some points, but it also reduces the number of BUGS in the game.
+
+NOTE: you can't have less than 0 points, and there will always be at least 3 bugs to challenge you.
+
+
+* GEMS:
+	- orange gem: +15 points, -1 bug
+	- green gem: +20 points, - 1 bug
+	- blue gem: +35 points, -1 bug
+
+Reaching the water: +50 points. +1 bug
+
+Happy gaming!
+
+## LICENSE
+
+MIT
--- js/app.js
+++ js/app.js
@@ -5,45 +5,51 @@ var allGems = [];
 
 var rawLocations = [-25, 55, 135, 215, 295, 375];
 var columnLocations = [0, 100, 200, 300, 400];
+var rawStart = rawLocations[5];
+var colStart = columnLocations[2];
+var rawTop = rawLocations[0];
+var rawEndPoint = columnLocations[4];
+var rawStartPoint = columnLocations[0];
 
 var bugCount = 0;
 var timeForFriend;
-var score = 0, scoreRecord = 0;
+var score = 0,
+    scoreRecord = 0;
 
 var playerSprites = [
     'images/char-boy.png',
     'images/char-cat-girl.png',
     'images/char-horn-girl.png',
     'images/char-pink-girl.png'
-    ];
+];
 
 var gemSprites = [
     'images/Gem Blue.png',
     'images/Gem Green.png',
     'images/Gem Orange.png'
-    ];
+];
 
 // Enemies our player must avoid
 var Enemy = function() {
-    this.x = 0;
-    this.y = rawLocations[randomInteger(1,3)];
-    this.speed = randomInteger(1,4);
+    this.x = rawStartPoint;
+    this.y = rawLocations[randomInteger(1, 3)];
+    this.speed = randomInteger(1, 4);
     this.sprite = 'images/enemy-bug.png';
 };
 
 
-//When enemy reaches raw end, it reappears on another randomized line.
+//This should maintain certain number of bugs in games
 Enemy.prototype.update = function(dt) {
-    if (this.x > 500) {
-        if (allEnemies.length <= enemyNumberTotal) {//Respawn bug code
-            this.x = columnLocations[0];
-            this.y = rawLocations[randomInteger(1,3)];
-            this.speed = randomInteger(1,4);
-        }
-        else {
+    if (this.x > rawEndPoint) { //end of raw
+        if (allEnemies.length <= enemyNumberTotal) {
+            this.x = rawStartPoint;
+            this.y = rawLocations[randomInteger(1, 3)];
+            this.speed = randomInteger(1, 4);
+        } else {
             allEnemies.splice(find(allEnemies, this), 1);
         };
 
+        //Number of bugs used as a time concept for Princess creation.
         bugCount++;
         if (bugCount == timeForFriend) {
             createFriend();
@@ -57,8 +63,11 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//Function below is used in 2 ways:
+//- to create total number of enemies at the start of the game,
+//- to add certain given num of enemies, when needed.
 var createEnemy = function(num) {
-    for (i = enemyNumberTotal - num; i < enemyNumberTotal ; i++) {
+    for (i = enemyNumberTotal - num; i < enemyNumberTotal; i++) {
         allEnemies.push(new Enemy());
     }
 };
@@ -73,12 +82,12 @@ Friend.prototype = Object.create(Enemy.prototype);
 Friend.prototype.constructor = Enemy;
 
 Friend.prototype.update = function(dt) {
-    if (this.x > 500) {
-      friendSetTimer();
-      allFriends=[];
+    if (this.x > rawEndPoint) {
+        friendSetTimer();
+        allFriends = [];
     };
 
-    this.x = this.x + this.speed * dt *100;
+    this.x = this.x + this.speed * dt * 100;
 };
 
 var createFriend = function() {
@@ -87,43 +96,43 @@ var createFriend = function() {
 };
 
 var friendSetTimer = function() {
-    timeForFriend = bugCount + randomInteger(3,12);
+    timeForFriend = bugCount + randomInteger(3, 12);
 };
 
-//Player related code
+//Player related code. Player starts from the center of the bottom raw
+var goToStart = function() {
+    this.x = colStart;
+    this.y = rawStart;
+};
+
 var player = function() {
-    this.x = 200;
-    this.y = 375;
-    this.sprite = playerSprites[randomInteger(0,3)];
+    goToStart.call(this);
+    this.sprite = playerSprites[randomInteger(0, 3)];
 };
 
 player.prototype.update = function() {
-    //When player reaches the water, game restarts with 1 more enemy
-    if (this.y == rawLocations[0]) {
-        this.y = 375;
+    //When player reaches the water, and game restarts with 1 more enemy
+    if (this.y == rawTop) {
+        this.y = rawStart;
         enemyNumberTotal++;
         createEnemy(1);
         score += 50;
     };
 
-    //When bug hits player, player goes back to start position
-    for (var i= 0; i < allEnemies.length; i++) {
+    //BUG hits the player
+    for (var i = 0; i < allEnemies.length; i++) {
         if (collision(this.x, this.y, allEnemies[i].x, allEnemies[i].y)) {
-            this.x = 200;
-            this.y = 375;
+            goToStart.call(this);
             if (score > 0) {
                 score -= 5;
             };
-            // if (enemyNumberTotal > 3) {
-            //     enemyNumberTotal -= 1;
-            // }
         }
     };
 
     //player picks up gem and scores
-    for (var i= 0; i < allGems.length; i++) {
+    for (var i = 0; i < allGems.length; i++) {
         if (collision(this.x, this.y, allGems[i].x, allGems[i].y)) {
-            switch(allGems[i].sprite) {
+            switch (allGems[i].sprite) {
                 case 'images/Gem Blue.png':
                     score += 35;
                     break;
@@ -135,45 +144,46 @@ player.prototype.update = function() {
             }
             allGems.splice(i, 1);
             friendSetTimer();
-            if (enemyNumberTotal > 3) {//TODO: Replace 3 with some variable
+
+            if (enemyNumberTotal > 3) { //At least 3 bugs in the game
                 enemyNumberTotal -= 1;
             }
         }
     };
 
     //if player meets Friend, gem appears
-    for (var i= 0; i < allFriends.length; i++) {
+    for (var i = 0; i < allFriends.length; i++) {
         if (collision(this.x, this.y, allFriends[i].x, allFriends[i].y)) {
-          allFriends=[];
-          new Gem();
+            allFriends = [];
+            new Gem();
         }
     }
 };
 
 player.prototype.handleInput = function(moveDirection) {
-    switch(moveDirection) {
+    switch (moveDirection) {
         case 'left':
-            if (this.x > 0) {
-                this.x = this.x -100;
-        }
-        break;
+            if (this.x > rawStartPoint) {
+                this.x -= 100;
+            }
+            break;
 
         case 'right':
-            if (this.x < 400) {
-                this.x = this.x + 100;
-        }
-        break;
+            if (this.x < rawEndPoint) {
+                this.x += 100;
+            }
+            break;
 
         case 'up':
-            if (this.y > -25) {
-                this.y = this.y - 80;
-        }
-        break;
+            if (this.y > rawTop) {
+                this.y -= 80;
+            }
+            break;
 
         case 'down':
-            if (this.y < 375) {
-                this.y = this.y + 80;
-        }
+            if (this.y < rawStart) {
+                this.y += 80;
+            }
     }
 };
 
@@ -183,20 +193,15 @@ player.prototype.render = function() {
 
 //Gem code goes here:
 var Gem = function() {
-  this.x = columnLocations[randomInteger(0,4)];
-  this.y = rawLocations[randomInteger(1,5)];
-  this.sprite = gemSprites[randomInteger(0,2)];
-  allGems.push(this);
+    this.x = columnLocations[randomInteger(0, 4)];
+    this.y = rawLocations[randomInteger(1, 5)];
+    this.sprite = gemSprites[randomInteger(0, 2)];
+    allGems.push(this);
 };
 
 Gem.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-//Actual creation of player, enemies, friends
-var player = new player();
-createEnemy(enemyNumberTotal);
-createFriend();
 
 //Helper function for randomizing
 function randomInteger(min, max) {
@@ -207,19 +212,26 @@ function randomInteger(min, max) {
 
 //Collision check
 var collision = function(playerX, playerY, x2, y2) {
-  var circle1 = {radius: 20, x: playerX, y: playerY};
-  var circle2 = {radius: 20, x: x2, y: y2};
-  var dx = circle1.x - circle2.x;
-  var dy = circle1.y - circle2.y;
-  var distance = Math.sqrt(dx * dx + dy * dy);
-  if (distance < circle1.radius + circle2.radius) {
-      return true;
-  }
-  return false;
+    var circle1 = {
+        radius: 20,
+        x: playerX,
+        y: playerY
+    };
+    var circle2 = {
+        radius: 20,
+        x: x2,
+        y: y2
+    };
+    var dx = circle1.x - circle2.x;
+    var dy = circle1.y - circle2.y;
+    var distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < circle1.radius + circle2.radius) {
+        return true;
+    }
+    return false;
 };
 
-
-// Code below is to keep track of score and to display it
+//Player score and score's record goes here.
 var scoreUpdate = function() {
     if (score > scoreRecord) {
         scoreRecord = score;
@@ -246,3 +258,7 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+var player = new player();
+createEnemy(enemyNumberTotal);
+createFriend();
